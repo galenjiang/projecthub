22105af593d097ea4bf83290fb6ec2974440cb17
galenjiang <249254832@qq.com>
2016-01-11 21:42:39 +0800

    update

--- test/react/assets/js/hello.js
+++ test/react/assets/js/hello.js
@@ -1,8 +1,9 @@
 var React = require('react');
+
 class Hello extends React.Component {
-  render() {
-    return (
-      <h1>Hello {this.props.name}!</h1>
-    );
-  }
-}
+    render() {
+      return ( < h1 > Hello {
+          this.props.name
+        }! < /h1>);
+      }
+    }
