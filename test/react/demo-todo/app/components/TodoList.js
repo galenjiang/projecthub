9c6e0c3960f5118c55157a0573f9f3aafb3ad30e
galen <249254832@qq.com>
2016-02-29 18:43:26 +0800

    update

--- test/react/demo-todo/app/components/TodoList.js
+++ test/react/demo-todo/app/components/TodoList.js
@@ -1,13 +1,20 @@
 import React from "react";
 
 export default React.createClass({
+  onClickHandler: function() {
+    this.props.onListClickHandler(this.props.itemKey);
+  },
+  onDelHandler: function() {
+    this.props.onDelHandler(this.props.itemKey)
+  },
   render: function(){
     return (
-      <div className="checkbox item-todolist" key = {this.props.key}>
+      <div className="checkbox item-todolist"
+        key={this.props.key}>
         <label className="label-todolist">
-          <input type="checkbox" value="" />
-          <button type="button" className="btn btn-default btn-del">删除</button>
-          <p className="status-del desc-item">
+          <input onClick={this.onClickHandler} type="checkbox" defaultChecked={this.props.done} />
+          <button onClick={this.onDelHandler} type="button" className="btn btn-default btn-del">删除</button>
+          <p className={this.props.done ? "status-del desc-item" : "desc-item"}>
             {this.props.text}
           </p>
         </label>
--- test/react/demo-todo/app/containers/ToDoMVC.js
+++ test/react/demo-todo/app/containers/ToDoMVC.js
@@ -1,21 +1,76 @@
 import React from 'react';
+import _ from "lodash";
 import ContentBody from "../components/ContentBody";
 import NavBar from "../components/NavBar";
+
 
 export default React.createClass({
   getInitialState: function(){
     return {
       todoLists: [
-        {key: 1,text: "hello, world"},
-        {key: 2,text: "hello, galen"}
+        {key: 1, text: "hello, world", done: true},
+        {key: 2,text: "hello, galen", done: false}
       ],
+      key: 2,
     }
   },
+  onNewList: function(newList) {
+    this.setState({key: this.state.key + 1});
+    let newTodoList = {
+      key: this.state.key + 1,
+      text: newList,
+      done: false,
+    }
+    let newTodoLists = this.state.todoLists.concat( newTodoList );
+    this.setState({
+      todoLists: newTodoLists,
+    })
+  },
+  onListClickHandler: function(itemKey) {
+    let copyTodoLists = _.uniq(this.state.todoLists);
+    let index = _.findIndex(copyTodoLists, function(item) {
+      return item.key == itemKey;
+    })
+    copyTodoLists[index].done ? copyTodoLists[index].done = false : copyTodoLists[index].done = true;
+    this.setState({
+      todoLists: copyTodoLists,
+    });
+  },
+  onDelHandler: function(itemKey) {
+    let copyTodoLists = _.uniq(this.state.todoLists);
+    let index = _.findIndex(copyTodoLists, function(item) {
+      return item.key == itemKey;
+    })
+    copyTodoLists.splice(index, 1);
+    this.setState({
+      todoLists: copyTodoLists
+    })
+  },
+  onDelAllHandler: function() {
+    let newTodoLists = _.filter(this.state.todoLists, function(item) {
+      return item.done !== true;
+    })
+    this.setState({
+      todoLists: newTodoLists
+    })
+  },
   render: function(){
+    let total = this.state.todoLists.length;
+    let doneList = _.filter(this.state.todoLists, function(item) {
+      return item.done == true
+    })
+    let done = doneList.length;
     return (
       <div>
         <NavBar />
-        <ContentBody todoLists = {this.state.todoLists} />
+        <ContentBody
+          todoLists={this.state.todoLists}
+          onNewList={this.onNewList}
+          onListClickHandler={this.onListClickHandler}
+          total={total}
+          done={done}
+          onDelHandler={this.onDelHandler}
+          onDelAllHandler={this.onDelAllHandler} />
       </div>
     )
   }
