c36c640b73dd3e4e9c390b7fd60528ded5b4c314
galen <249254832@qq.com>
2016-03-08 19:18:22 +0800

    'update'

--- test/react/demoredux/app/stores/configureStore.js
+++ test/react/demoredux/app/stores/configureStore.js
@@ -1,12 +1,26 @@
-import { createStore } from 'redux';
-import rootReducer from '../reducers';
-export default function configureStore(initialState) {
-  const store = createStore(rootReducer, initialState);
-  if (module.hot) {
-    module.hot.accept('../reducers', () => {
-      const nextReducer = require('../reducers');
-      store.replaceReducer(nextReducer);
-    });
-  }
-  return store;
-}
+import { createStore } from 'redux'
+import todoApp from '../reducers/reducer'
+let store = createStore(todoApp)
+
+
+import { addTodo, completeTodo, setVisibilityFilter, VisibilityFilters } from '../actions/actions';
+
+// 打印初始状态
+console.log(store.getState());
+
+// 监听 state 更新时，打印日志
+// 注意 subscribe() 返回一个函数用来注销监听器
+let unsubscribe = store.subscribe(() =>{}
+  console.log(store.getState())
+);
+
+// 发起一系列 action
+store.dispatch(addTodo('Learn about actions'));
+store.dispatch(addTodo('Learn about reducers'));
+store.dispatch(addTodo('Learn about store'));
+store.dispatch(completeTodo(0));
+store.dispatch(completeTodo(1));
+store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED));
+
+// 停止监听 state 更新
+unsubscribe();
