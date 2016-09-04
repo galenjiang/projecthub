af1a89a69adf354d68d12cf65df9155908de4b30
galenjiang <249254832@qq.com>
2016-09-04 14:47:45 +0800

    sandel

--- test/vue/demo1/app.js
+++ test/vue/demo1/app.js
@@ -1,7 +1,31 @@
+Vue.config.debug = true
+
+// Vue.transition('fade',{
+//   beforeEnter: function(){
+//     console.log(111)
+//   },
+//   beforeLeave: function(){
+//     console.log(222)
+//   }
+// })
+
+Vue.transition('expand', {
+
+  beforeEnter: function (el) {
+    console.log(111)
+  }
+})
+
 new Vue({
   el: '#app',
   data: {
-    checkedNames: []
+    checkedNames: [],
+    testShow: true
+  },
+  methods: {
+    change: function(){
+      this.testShow ? this.testShow = false : this.testShow = true
+    }
   }
 
 })
